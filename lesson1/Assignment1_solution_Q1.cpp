d00438e7061567de48c0cd3a96a56a3cc41849d5
moajouz <moajouz.work@gmail.com>
2024-08-05 08:59:15 +0300

    new file:   General_Info/cpp-data-types.png 	new file:   General_Info/cpp-operaters.png 	new file:   lesson.cpp 	new file:   lesson1/Assignment1_1.png 	new file:   lesson1/Assignment1_2.png 	new file:   lesson1/Assignment1_solution_Q1.cpp

---
+++ lesson1/Assignment1_solution_Q1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main(){
+    int n1, n2, n3, sum, product;
+    std::cout<<"enter 1st number: ";
+    std::cin>>n1;
+    std::cout<<"enter 2st number: ";
+    std::cin>>n2;
+    std::cout<<"enter 3rd number: ";
+    std::cin>>n3;
+    sum= n1+n2+n3;
+    product= n1*n2*n3;
+    std::cout<<"sum is: "<< sum<<std::endl;
+    std::cout<<"product is: "<< product<<std::endl;
+    return 0;
+}
